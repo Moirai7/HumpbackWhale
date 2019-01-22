61f5fae0d8f2368255a83825e8c13ef5d2f869e8
x <1>
2019-01-22 13:48:35 +0800

    xu

--- identification/reid/trainers_partloss.py
+++ identification/reid/trainers_partloss.py
@@ -72,7 +72,7 @@ class Trainer(BaseTrainer):
         imgs, pids = inputs
         inputs = [Variable(imgs)]
         #targets = Variable(pids)
-        
+        print(pids)
         targets = Variable(torch.FloatTensor(list(map(int, pids))).cuda())
         return inputs, targets
 
