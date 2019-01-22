5262bae34c824e228f44f00ee38743d0308c962d
x <1>
2019-01-22 15:32:27 +0800

    xu

--- identification/train_PCB.sh
+++ identification/train_PCB.sh
@@ -1,2 +1,2 @@
-CUDA_VISIBLE_DEVICES=0 python loaddata.py -d market -a resnet50 -b 64 -j 4 --epochs 60 --log logs/market-1501/PCB/ --combine-trainval --feature 256 --height 256 --width 256 --step-size 40 --data-dir ~/HumpbackWhale/dataset/
+CUDA_VISIBLE_DEVICES=0 python loaddata.py -d market -a resnet50 -b 32 -j 4 --epochs 60 --log logs/humpbackWhale/ --combine-trainval --feature 256 --height 256 --width 256 --step-size 40 --data-dir ~/HumpbackWhale/dataset/
  
