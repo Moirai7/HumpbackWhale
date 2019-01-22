6996cc236fd9b35a5c15360fe7ef5468f7914ae4
Moirai7 <moirai.zhang@gmail.com>
2019-01-22 21:35:49 +0800

    update newdata

--- preprocess/requirement.sh
+++ preprocess/requirement.sh
@@ -1,3 +1,5 @@
 pip install six numpy scipy Pillow matplotlib scikit-image opencv-python imageio Shapely --user
 pip install imgaug --user
+mkdir ../newdataset
+mkdir ../newdataset/train
 python augment.py
