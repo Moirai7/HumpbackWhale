1729632a62624f56040a603dbd4534f5871e0743
x <1>
2019-01-23 18:04:36 +0800

    xu

--- identification/reid/utils/data/preprocessor.py
+++ identification/reid/utils/data/preprocessor.py
@@ -41,7 +41,7 @@ class HW_Test_Dataset(object):
         self.image_list = [x for x in os.listdir(self.file_path)]
 
     def __len__(self):
-        return (len(self.image_list))
+        return (len(self.image_list)-10)
 
     def __getitem__(self, indices):
         if isinstance(indices, (tuple, list)):
@@ -49,17 +49,16 @@ class HW_Test_Dataset(object):
         return self._get_single_item(indices)
 
     def _get_single_item(self, idx):
-        if idx < self.__len__()
-            img_path = os.path.join(self.file_path, self.df.Image[idx])
-            label = self.df.Id[idx]
-            new_label = self.df.Id[idx]
+        img_path = os.path.join(self.file_path, self.df.Image[idx])
+        label = self.df.Id[idx]
+        new_label = self.df.Id[idx]
 
         # img = cv2.imread(img_path)
         # img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-            imgs = Image.open(img_path)
+        imgs = Image.open(img_path)
 
-            imgs = imgs.convert('RGB')
-            imgs = self.transform(imgs)
+        imgs = imgs.convert('RGB')
+        imgs = self.transform(imgs)
 
         return imgs, label, new_label
 
