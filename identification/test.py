3f89ba918cafff66b1380810865f19a75eac848f
x <1>
2019-01-24 02:09:02 +0800

    xu

--- identification/test.py
+++
@@ -1,4 +0,0 @@
-start_epoch = 100
-best_top1 = 250
-print("=> Start epoch {}  best top1 {:.1f}"
-              .format(start_epoch, best_top1))
--- preprocess/requirement.sh
+++
@@ -1,4 +0,0 @@
-pip install six numpy scipy Pillow matplotlib scikit-image opencv-python imageio Shapely --user
-pip install imgaug --user
-mkdir ../newdataset
-mkdir ../newdataset/train
