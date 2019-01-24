2101200537d59105e4ec01ced5578ed9f0abc7ad
xu <xutongkun1208@ml-instance-xutongkun1208.c.ml-winter-camp-pek.internal>
2019-01-24 06:19:00 +0000

    xu

--- preprocess/label.py
+++ preprocess/label.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pandas as pd
 
-data = pd.read_csv('../dataset/train.csv')
+data = pd.read_csv('../identification/label2.csv')
 
 label = data['Id'].drop_duplicates()
 
@@ -9,6 +9,6 @@ data['newId'] = data['Id']
 for l, i in zip(label, range(len(label))):
    data.loc[data['newId']==l,'newId'] = i
 
-data.to_csv('../dataset/label.csv',index= False)
+data.to_csv('../dataset/label2.csv',index= True)
 
      
